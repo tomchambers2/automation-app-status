592070dc0865d1de3d18def593441bd50d9f115c
Michiel van der Velde <michiel@michielvdvelde.nl>
2014-07-26 13:22:14 +0200

    Added 'deny_publish' option

--- proxy.js
+++ proxy.js
@@ -22,7 +22,8 @@ server.on('connection', function(client) {
 		return allow(false);
 	});
 	client.on('publish', function(topic, message, flags, allow) {
-		if((!proxy_config.topics_allow || (proxy_config.topics_allow && isIn(proxy_config.topics_allow, topic))) ||
+		if(!proxy_config.deny_publish ||
+		(!proxy_config.topics_allow || (proxy_config.topics_allow && isIn(proxy_config.topics_allow, topic))) ||
 		(!proxy_config.topics_deny || (proxy_config.topics_deny && isIn(proxy_config.topics_deny, topic))))
 			return allow(true);
 		return allow(false);
